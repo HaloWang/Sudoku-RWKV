c8b1eff6718494cfd61744df3942c3bda6e420a1
WangCe <halowang1991@gmail.com>
2024-11-01 11:26:27 +0800

    check env

---
+++ check.py
@@ -0,0 +1,8 @@
+import torch
+
+print(torch.__version__)
+
+print("Is CUDA available?", torch.cuda.is_available())
+print("CUDA Available: ", torch.cuda.is_available())
+print("CUDA Device Count: ", torch.cuda.device_count())
+print("CUDA Device Name: ", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No CUDA device")
